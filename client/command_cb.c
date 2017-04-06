69e1d3f68db826f43d897aa0688c7a65b5829867
Juho Rinta-Paavola <juhorp@juhorp.fi>
2017-04-06 18:24:10 +0300

    Send a message to the current channel when a /-command is not given

--- client/command_cb.c
+++ client/command_cb.c
@@ -8,6 +8,8 @@
 #include "command_cb.h"
 #include "message.h"
 
+static char currentChannel[128] = "";
+
 void command_cb(struct bufferevent *bev_stdin, void *ctx)
 {
   struct bufferevent *bev = ctx;
@@ -18,21 +20,40 @@ void command_cb(struct bufferevent *bev_stdin, void *ctx)
   char *buf = evbuffer_readln(evbuf, NULL, EVBUFFER_EOL_ANY);
 
   while (buf) {
+    struct message msg;
     if (*buf == '/') {
       // Send the command straight to the server if it is valid
-      struct message msg = parseMessage(buf + 1);
+      msg = parseMessage(buf + 1);
       if (msg.type != MSG_INVALID) {
-        char *str = stringifyMessage(msg);
-        bufferevent_write(bev, str, strlen(str));
-        free(str);
+        if (msg.type == MSG_JOIN) {
+          char *channel = msg.argv[msg.argc - 1];
+          if (*channel != ':') {
+            strncpy(currentChannel, msg.argv[msg.argc - 1], 128);
+          } else {
+            strncpy(currentChannel, msg.argv[msg.argc - 1] + 1, 128);
+          }
+        }
       } else {
         puts("Invalid command");
       }
-      freeMessage(msg);
     } else {
-      puts("TODO: Handle sending messages");
+      // Send the input as a PRIVMSG to the current channel
+      msg.source = NULL;
+      msg.type = MSG_PRIVMSG;
+      msg.argv = malloc(2 * sizeof(char *));
+      msg.argv[0] = malloc((strlen(currentChannel) + 1) * sizeof(char));
+      strcpy(msg.argv[0], currentChannel);
+      msg.argv[1] = malloc((strlen(buf) + 2) * sizeof(char));
+      msg.argv[1][0] = ':';
+      strcpy(msg.argv[1] + 1, buf);
+      msg.argc = 2;
     }
 
+    char *str = stringifyMessage(msg);
+    bufferevent_write(bev, str, strlen(str));
+
+    free(str);
+    freeMessage(msg);
     free(buf);
     buf = evbuffer_readln(evbuf, NULL, EVBUFFER_EOL_ANY);
   }
