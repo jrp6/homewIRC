e280a0ce7c6cf5c576408aa87acf3fc1223ec27a
Juho Rinta-Paavola <juhorp@juhorp.fi>
2017-04-07 15:36:41 +0300

    Dereference return values of tsearch and tfind

--- server/connection.c
+++ server/connection.c
@@ -1,5 +1,6 @@
 #include <search.h>
 #include <stdlib.h>
+#include <string.h>
 #include "connection.h"
 
 //N.B. For good performance, ensure that your C library implements tsearch etc using balanced trees
@@ -17,12 +18,12 @@ static int compareConnections(const void *leftv, const void *rightv)
   return left->id - right->id;
 }
 
-static struct connection dummyConn(unsigned int id)
+static struct connection * dummyConn(unsigned int id)
 {
-  struct connection conn;
-  conn.id = id;
-  conn.nick = NULL;
-  conn.hostmask = NULL;
+  struct connection *conn = malloc(sizeof(struct connection));
+  conn->id = id;
+  conn->nick = NULL;
+  conn->hostmask = NULL;
   return conn;
 }
 
@@ -34,21 +35,25 @@ void removeConnection(const unsigned int id)
   struct connection *toRemove = getConnection(id);
   if (toRemove) {
     freeConnection(*toRemove);
-    struct connection c = dummyConn(id);
-    tdelete(&c, &root, compareConnections);
+    struct connection *c = dummyConn(id);
+    tdelete(c, &root, compareConnections);
+    free(c);
   }
 }
 
 struct connection * newConnection(void)
 {
-  struct connection c = dummyConn(nConns++);
-  return (struct connection *)tsearch(&c, &root, compareConnections);
+  struct connection *c = dummyConn(nConns++);
+  struct connection **ct = tsearch(c, &root, compareConnections);
+  return *ct;
 }
 
 struct connection * getConnection(const unsigned int id)
 {
-  struct connection c = dummyConn(id);
-  return tfind(&c, &root, compareConnections);
+  struct connection *c = dummyConn(id);
+  struct connection **ct = tfind(c, &root, compareConnections);
+  free(c);
+  return *ct;
 }
 
 unsigned int getNConns(void)
